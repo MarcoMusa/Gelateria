e904e793bba7d3411fbf668ec51e9300447bd5c6
Marco Musa <marco.musa91@gmail.com>
2021-07-07 12:37:20 +0200

    fixed navbar

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ let navbarCTAScroll = document.querySelector('#navbarCTAScroll')
 
 if (window.innerWidth > 576) {
 
-
     document.addEventListener('scroll', () => {
 
         if (window.pageYOffset > 20) {
             navbar.classList.add('bg-white', 'shadow')
+            navbarBrand.classList.add('text-yellow')
         } else {
             navbar.classList.remove('bg-white', 'shadow')
             navbar.classList.add('bg-transparent')
+        }
+
+        if (window.pageYOffset > window.innerWidth) {
+
+            navbarCTAScroll.classList.remove('d-none')
+
+        } else {
+
+            navbarCTAScroll.classList.add('d-none')
         }
 
 
